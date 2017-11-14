f6d09716c55f23b6ba7c415c61e34002c9542924
jjant <jantonielli@itba.edu.ar>
2017-11-14 11:41:55 -0300

    Resolve overflow on debug.c

--- src/transformer/debug.c
+++ src/transformer/debug.c
@@ -4,35 +4,38 @@
 #include "debug.h"
 
 char * state_to_string(state_type state) {
-  char * str = (char *)malloc(20 * sizeof(char));
+  char * ret_str;
+  char * str;
 
   if (state == HEADER_NAME) {
-    strcpy(str, "HEADER_NAME");
+    str = "HEADER_NAME";
   }
   if (state == HEADER_VALUE) {
-    strcpy(str, "HEADER_VALUE");
+    str = "HEADER_VALUE";
   }
   if (state == CONTENT_TYPE_TYPE) {
-    strcpy(str, "CONTENT_TYPE_TYPE");
+    str = "CONTENT_TYPE_TYPE";
   }
   if (state == CONTENT_TYPE_SUBTYPE) {
-    strcpy(str, "CONTENT_TYPE_SUBTYPE");
+    str = "CONTENT_TYPE_SUBTYPE";
   }
   if (state == ATTRIBUTE_NAME) {
-    strcpy(str, "ATTRIBUTE_NAME");
+    str = "ATTRIBUTE_NAME";
   }
   if (state == ATTRIBUTE_VALUE) {
-    strcpy(str, "ATTRIBUTE_VALUE");
+    str = "ATTRIBUTE_VALUE";
   }
   if (state == BOUNDARY) {
-    strcpy(str, "BOUNDARY");
+    str = "BOUNDARY";
   }
   if (state == CONTENT_TYPE_BODY) {
-    strcpy(str, "CONTENT_TYPE_BODY");
+    str = "CONTENT_TYPE_BODY";
   }
   if (state == POSSIBLE_ERROR) {
-    strcpy(str, "POSSIBLE_ERROR");
+    str = "POSSIBLE_ERROR";
   }
 
-  return str;
+  ret_str = malloc((strlen(str) + 2) * sizeof(char));
+  strcpy(ret_str, str);
+  return ret_str;
 }
